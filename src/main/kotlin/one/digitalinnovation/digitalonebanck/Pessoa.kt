aee1940221c81fb451249219e188c9913e69b282
Anderson <andersons1lva@yahoo.com.br>
2022-06-27 17:32:47 -0300

    Initial commit

---
+++ src/main/kotlin/one/digitalinnovation/digitalonebanck/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digitalonebanck
+
+class Pessoa {
+    var nome: String = " Anderson"
+    var cpf: String = "111.552.996-15"
+}
+
+fun main(){
+    val Anderson = Pessoa()
+
+    println(Anderson.nome)
+    println(Anderson.cpf)
+}
