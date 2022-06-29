639dbd949d12f117b15ebd05482df8065773387f
Anderson <andersons1lva@yahoo.com.br>
2022-06-28 21:32:47 -0300

    created account date class

---
+++ src/main/kotlin/one/digitalinnovation/digitalonebanck/testes/BancoTeste.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digitalonebanck.testes
+
+import one.digitalinnovation.digitalonebanck.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome = "DigiOne", numero = 12)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val banco2 = digiOneBank.copy(nome = "Banco2")
+
+    println(banco2.info())
+}
