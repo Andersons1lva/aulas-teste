0e2ece06fe3dcd0ee79d2c4f42eab62c790b3d5d
Anderson <andersons1lva@yahoo.com.br>
2022-06-30 23:54:25 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digitalonebanck/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digitalonebanck/Pessoa.kt
@@ -1,20 +1,6 @@
 package one.digitalinnovation.digitalonebanck
 
-class Pessoa {
-    var nome: String = " Anderson"
-    var cpf: String = "111.552.996-15"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-
-
-}
-
-fun main(){
-    val Anderson = Pessoa()
-
-    println(Anderson.pessoaInfo())
-
-}
+ abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
