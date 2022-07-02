5eb5f0bf2d8233f85b0beecd48f562ec59a8ffee
Anderson <andersons1lva@yahoo.com.br>
2022-07-02 19:05:18 -0300

    update enploye class

--- src/main/kotlin/one/digitalinnovation/digitalonebanck/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digitalonebanck/Funcionario.kt
@@ -1,11 +1,16 @@
 package one.digitalinnovation.digitalonebanck
 
-import java.math.BigDecimal
-
- abstract class Funcionario(
-     nome: String,
-     cpf: String,
-    val salario: BigDecimal
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
+    val salario: Double,
 ) : Pessoa(nome, cpf) {
-    abstract fun calculoauxilio():
+   protected abstract fun calculoauxilio(): Double
+    override fun toString(): String = """
+        Nome: $nome
+        Cpf: $cpf
+        Salario: $salario
+        Auxilio: ${calculoauxilio()}
+    """.trimIndent()
+
 }
